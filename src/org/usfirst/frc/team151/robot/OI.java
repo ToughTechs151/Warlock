1c0345cc86d78047f5ac85e5bd4813d78f89c0dc
usd17176 <usd17176@USDANRMMD1NBP28.code1.emi.philips.com>
2017-01-24 22:09:27 -0500

    fixed null exception

--- src/org/usfirst/frc/team151/robot/OI.java
+++ src/org/usfirst/frc/team151/robot/OI.java
@@ -8,32 +8,35 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	Joystick joystick = null;	
-	//TODO initialize the other buttons in the OI constructor
-	private JoystickButton x = new JoystickButton(joystick, 1);
-	private JoystickButton a = new JoystickButton(joystick, 2);
-	private JoystickButton b = new JoystickButton(joystick, 3);
-	private JoystickButton y = new JoystickButton(joystick, 4);
+	Joystick joystick = null;
+	
+	private JoystickButton x = null;
+	private JoystickButton a = null;
+	private JoystickButton b = null;
+	private JoystickButton y = null;
 	private JoystickButton leftBumper = null;
-	private JoystickButton rightBumper = null;
+	private  JoystickButton rightBumper = null;
 	private JoystickButton leftTrigger = null;
 	private JoystickButton rightTrigger = null;
 	private JoystickButton back = null;
 	private JoystickButton start = null;
 	private JoystickButton leftJoyDown = null;
-	private JoystickButton rightJoyDown = null;
+	private JoystickButton rightJoyDown = null;	
 	
 	public OI(int joystickChannel) {		
-		joystick = new Joystick(joystickChannel);
-		leftBumper = new JoystickButton(joystick, 5);
-		rightBumper = new JoystickButton(joystick, 6);
-		leftTrigger = new JoystickButton(joystick, 7);
-		rightTrigger = new JoystickButton(joystick, 8);
-		back = new JoystickButton(joystick, 9);
-		start = new JoystickButton(joystick, 10);
-		leftJoyDown = new JoystickButton(joystick, 11);
-		rightJoyDown = new JoystickButton(joystick, 12);	
-//		y.whenPressed(new ClimbRopeCommand());
+		joystick = new Joystick(joystickChannel);	
+		JoystickButton x = new JoystickButton(joystick, 1);
+		JoystickButton a = new JoystickButton(joystick, 2);
+		JoystickButton b = new JoystickButton(joystick, 3);
+		JoystickButton y = new JoystickButton(joystick, 4);
+		JoystickButton leftBumper = new JoystickButton(joystick, 5);
+		JoystickButton rightBumper = new JoystickButton(joystick, 6);
+		JoystickButton leftTrigger = new JoystickButton(joystick, 7);
+		JoystickButton rightTrigger = new JoystickButton(joystick, 8);
+		JoystickButton back = new JoystickButton(joystick, 9);
+		JoystickButton start = new JoystickButton(joystick, 10);
+		JoystickButton leftJoyDown = new JoystickButton(joystick, 11);
+		JoystickButton rightJoyDown = new JoystickButton(joystick, 12);		
 	}
 	
     public Joystick getJoystick() {
@@ -47,9 +50,9 @@ public class OI {
     		case 3: return b;
     		case 4: return y;
     		case 5: return leftBumper;
-    		case 6: return rightBumper; 
+    		case 6 : return rightBumper; 
     		case 7: return leftTrigger;
-    		case 8: return rightTrigger;
+    		case 8 : return rightTrigger;
     		case 9: return back;
     		case 10: return start;
     		case 11: return leftJoyDown;
