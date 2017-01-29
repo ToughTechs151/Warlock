e545c377387aafd561b4b70d936e7a397059b0c8
Readingpanther5 <foti.grant@gmail.com>
2017-01-29 12:47:33 -0500

    Update DriverOI.java

--- src/org/usfirst/frc/team151/robot/DriverOI.java
+++ src/org/usfirst/frc/team151/robot/DriverOI.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team151.robot;
 
+import org.usfirst.frc.team151.robot.commands.PickUpCommand;
+import org.usfirst.frc.team151.robot.commands.ReversePickupCommand;
+import org.usfirst.frc.team151.robot.commands.StopPickupCommand;
+
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
@@ -12,9 +16,16 @@ public class DriverOI extends OI{
 	public DriverOI(int joystickChannel) {
 		super(joystickChannel);
 		// TODO Auto-generated constructor stub
+		leftBumper = new JoystickButton(joystick, 5);
 		rightBumper = new JoystickButton(joystick, 6);
 		leftTrigger = new JoystickButton(joystick, 7);
-		rightTrigger = new JoystickButton(joystick, 8);
+		rightTrigger = new JoystickButton(joystick, 8); 
+		back = new JoystickButton(joystick, 9);
+		
+		//checks to see if buttons are pressed
+		leftTrigger.whenPressed(new PickUpCommand());
+		leftBumper.whenPressed(new StopPickupCommand());
+		back.whenPressed(new ReversePickupCommand());
 	}
 
 }
