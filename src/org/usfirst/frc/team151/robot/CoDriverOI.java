9517a0fa3694226a11becf97731736e02f5a9892
Kevin <kcq@yahoo.com>
2017-01-29 08:14:57 -0500

    Added drivers classes so that we can seperate the specific logics to control the Joystick for the drivers

---
+++ src/org/usfirst/frc/team151/robot/CoDriverOI.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team151.robot;
+
+
+/**
+ * 
+ * This class encapsulates the logics to manage the secondary or Co-driver Joystick controls
+ *
+ */
+public class CoDriverOI extends OI{
+
+	public CoDriverOI(int joystickChannel) {
+		super(joystickChannel);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/org/usfirst/frc/team151/robot/DriverOI.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team151.robot;
+
+/**
+ * 
+ * This class encapsulates the logics to manage the primary driver Joystick controls
+ *
+ */
+public class DriverOI extends OI{
+
+	public DriverOI(int joystickChannel) {
+		super(joystickChannel);
+		// TODO Auto-generated constructor stub
+	}
+
+}
