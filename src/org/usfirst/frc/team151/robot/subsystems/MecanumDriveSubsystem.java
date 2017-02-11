d9dd0aa44db711a22fde52bd097c518c8b134930
151_2 <151_2@Pippin>
2017-02-11 11:28:36 -0500

    2/11/17 - PID Controllers

--- src/org/usfirst/frc/team151/robot/subsystems/MecanumDriveSubsystem.java
+++ src/org/usfirst/frc/team151/robot/subsystems/MecanumDriveSubsystem.java
@@ -1,10 +1,12 @@
 package org.usfirst.frc.team151.robot.subsystems;
 
 import org.usfirst.frc.team151.robot.OI;
+import org.usfirst.frc.team151.robot.Robot;
 import org.usfirst.frc.team151.robot.RobotMap;
 import org.usfirst.frc.team151.robot.commands.DriveWithJoystickCommand;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -24,17 +26,35 @@ public class MecanumDriveSubsystem extends Subsystem {
 	private SpeedController leftReartSpeedController = null;
 	private RobotDrive robotDrive = null;
 	
+	private Encoder leftFrontEncoder = null;
+	private Encoder rightFrontEncoder = null;
+	private Encoder rightRearEncoder = null;
+	private Encoder leftRearEncoder = null;
+	
 	public static ADXRS450_Gyro gyro = null;
 	
 	public static final double normalMultiplier = 0.5;
 	public static final double creepMultiplier = 0.25;
 	public static final double turboMultiplier = 1.0;
+	
+	public static final double totalDistance = 72.0;
+	public static final double distancePerPulse = 6 * Math.PI / 1440; //in inches, 1440 pulses
 
 	public MecanumDriveSubsystem() {
 		leftFrontSpeedController = new Talon(RobotMap.leftFrontMotor);        
 		rightFrontSpeedController = new Talon(RobotMap.rightFrontMotor);
 		rightRearSpeedController = new Talon(RobotMap.rightRearMotor);
 		leftReartSpeedController = new Talon(RobotMap.leftRearMotor);
+		
+		leftFrontEncoder = new Encoder(0, 1); //check values
+		rightFrontEncoder = new Encoder(2, 3);
+		rightRearEncoder = new Encoder(4, 5);
+		leftRearEncoder = new Encoder(6, 7);
+		
+		leftFrontEncoder.setDistancePerPulse(distancePerPulse);
+		rightFrontEncoder.setDistancePerPulse(distancePerPulse);
+		leftRearEncoder.setDistancePerPulse(distancePerPulse);
+		rightRearEncoder.setDistancePerPulse(distancePerPulse);
 		
 		gyro = new ADXRS450_Gyro();
 		gyro.calibrate();
@@ -92,5 +112,22 @@ public class MecanumDriveSubsystem extends Subsystem {
 	 */
 	public void log() {
 		SmartDashboard.putData("Gyro", gyro);
+	}
+	
+	public double getDistanceTraveled() {
+		return ((leftFrontEncoder.getDistance() + rightFrontEncoder.getDistance() + 
+				leftRearEncoder.getDistance() + rightRearEncoder.getDistance())/4);
+	}
+	
+	public double getDistanceRemaining() {
+		return (totalDistance - getDistanceTraveled());
+	}
+	
+	public void reset() {
+		leftRearEncoder.reset();
+		rightRearEncoder.reset();
+		rightFrontEncoder.reset();
+		leftRearEncoder.reset();
+		gyro.reset();
 	}
 }
